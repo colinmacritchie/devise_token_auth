54337fad124424dcb36eeafeffd0bee2aa741e99
mcolin <colinmacritchie@users.noreply.github.com>
2015-11-09 23:01:12 -0700

    Update

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -14,11 +14,11 @@ module DeviseTokenAuth
         @resource.email = sign_up_params[:email]
       end
 
-      # success redirect url is required
-      if resource_class.devise_modules.include?(:confirmable) && !params[:confirm_success_url]
+         # success redirect url is required
+      unless params[:confirm_success_url]
         return render json: {
           status: 'error',
-          data:   @resource.as_json,
+          data:   @resource,
           errors: ["Missing `confirm_success_url` param."]
         }, status: 403
       end
@@ -58,7 +58,7 @@ module DeviseTokenAuth
           clean_up_passwords @resource
           render json: {
             status: 'error',
-            data:   @resource.as_json,
+            data:   @resource,
             errors: @resource.errors.to_hash.merge(full_messages: @resource.errors.full_messages)
           }, status: 403
         end
@@ -66,56 +66,8 @@ module DeviseTokenAuth
         clean_up_passwords @resource
         render json: {
           status: 'error',
-          data:   @resource.as_json,
+          data:   @resource,
           errors: ["An account already exists for #{@resource.email}"]
         }, status: 403
       end
     end
-
-    def update
-      if @resource
-
-        if @resource.update_attributes(account_update_params)
-          render json: {
-            status: 'success',
-            data:   @resource.as_json
-          }
-        else
-          render json: {
-            status: 'error',
-            errors: @resource.errors
-          }, status: 403
-        end
-      else
-        render json: {
-          status: 'error',
-          errors: ["User not found."]
-        }, status: 404
-      end
-    end
-
-    def destroy
-      if @resource
-        @resource.destroy
-
-        render json: {
-          status: 'success',
-          message: "Account with uid #{@resource.uid} has been destroyed."
-        }
-      else
-        render json: {
-          status: 'error',
-          errors: ["Unable to locate account for destruction."]
-        }, status: 404
-      end
-    end
-
-    def sign_up_params
-      params.permit(devise_parameter_sanitizer.for(:sign_up))
-    end
-
-    def account_update_params
-      params.permit(devise_parameter_sanitizer.for(:account_update))
-    end
-  end
-end
